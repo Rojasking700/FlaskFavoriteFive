be9552c1367a9a6fc00d447df8e8b6a82b4edd32
gabriel rojas <grojaspiano@gmail.com>
2021-03-01 22:54:54 -0600

    fourth maybe fifth commit, idk -GR

---
+++ README.txt
@@ -0,0 +1,9 @@
+I tried to do this using classes and object so i could add many artist and to one genre
+automatically but couldnt seem to figure it out. This is the latest working version im 
+sure we will go over how to do integrate objects with this later.
+
+
+as of now there is nothing in the index but by using the nav bar you can go through my 
+favorite genres and top artist
+
+ps sorry for any typos in this read me i just gave up by now
--- app/routes.py
+++ app/routes.py
@@ -3,7 +3,7 @@ from flask import render_template
 
 class Artist():
 
-    def __init__(self, genre ,artist, s1, s2, s3, s4, s5):
+    def __init__(self, genre ,artist, s1, s2, s3, s4, s5): #this intializes all of the data
         self.genre = genre
         self.artist = artist
         self.s1 = s1
@@ -12,7 +12,7 @@ class Artist():
         self.s4 = s4
         self.s5 = s5
 
-    def dict(self):
+    def dict(self): # this creates a dictionary that can be used in the html to refernce all the data
         
         dic = {
             'title': self.genre,
@@ -23,8 +23,14 @@ class Artist():
             # 's4': 
             # 's5': 
         }
-        if self.genre == 'rock':
+        if self.genre == 'rock': # if genre  = rock then it will add it to the rock genre
             rocks[self.artist]= dic
+        elif self.genre == 'rap':
+            raps[self.artist]= dic
+        elif self.genre == 'country':
+            countrys[self.artist]= dic
+        elif self.genre == 'mexican':
+            mexicans[self.artist]= dic
         # print(rock)
         return dic
 
@@ -36,27 +42,43 @@ def hello_world():
 
     title = "Hello World"
     return render_template('index.html', title=title)
-
+#************************************************************
+raps = {
+    'title': 'rap' #this diction is for all of the artist of the rap genre
+}
 @app.route('/rap')
 def rap():
+    trippie = Artist('rap','trippie_redd','Bang','Taking a Walk','Weee','Dark Knight','Shake it Up')
+    trippie.dict()
     title = 'Rap'
-    return render_template('rap.html', title=title)
+    return render_template('rap.html', **raps)
+#************************************************************
 rocks = {
-    'title': 'rock'
+    'title': 'rock' #this diction is for all of the artist of the rock genre
 }
 @app.route('/rock')
 def rock():
-    motionless = Artist('rock','Motionless_in_White','Another Life','Somebody Told Me','Eternally Yours','Disguise','Voices') 
-    motionless.dict()
+    motionless = Artist('rock','Motionless_in_White','Another Life','Somebody Told Me','Eternally Yours','Disguise','Voices') #this intializes the band motionless in white
+    motionless.dict() #this runs the dict function
     title = 'Rock'
     return render_template('rock.html', **rocks)
-
+#************************************************************
+countrys = {
+    'title': 'country' #this diction is for all of the artist of the rock genre
+}
 @app.route('/country')
 def country():
+    Morgan = Artist('country','Morgan_Wallen','7 summers','whiskey glasses','Chasin you','Wasted on You','865')
+    Morgan.dict()
     title = 'Country'
-    return render_template('country.html', title=title)
-
+    return render_template('country.html', **countrys)
+#************************************************************
+mexicans = {
+    'title': 'mexican' #this diction is for all of the artist of the rock genre
+}
 @app.route('/mexican')
 def mexican():
+    t3r = Artist('mexican','T3R_Elemento','Ojitos de conejo','Garantia de Calidad','El Oso', 'Amigo de la Muerte', 'Jalo y exajalo')
+    t3r.dict()
     title = 'Mexican'
-    return render_template('mexican.html', title=title)
+    return render_template('mexican.html', **mexicans)
