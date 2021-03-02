e32c0fa610f54fb8883ab386a1cee901ac4c87a9
gabriel rojas <grojaspiano@gmail.com>
2021-03-01 18:03:55 -0600

    third commit -GR

--- app/routes.py
+++ app/routes.py
@@ -7,3 +7,23 @@ from flask import render_template
 def hello_world():
     title = "Hello World"
     return render_template('index.html', title=title)
+
+@app.route('/rap')
+def rap():
+    title = 'Rap'
+    return render_template('rap.html', title=title)
+
+@app.route('/rock')
+def rock():
+    title = 'Rock'
+    return render_template('rock.html', title=title)
+
+@app.route('/country')
+def country():
+    title = 'Country'
+    return render_template('country.html', title=title)
+
+@app.route('/mexican')
+def mexican():
+    title = 'Mexican'
+    return render_template('mexican.html', title=title)
